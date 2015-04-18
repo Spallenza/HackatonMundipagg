684b8217fd551ba4eb243781f51ff88d74de4051
Spallenza <diegoreis@gmail.com>
2015-04-17 22:56:54 -0300

    Implementação do GetMerchantOrderId

--- Hackaton/Hackaton.Core/Repository/TestRepository.cs
+++ Hackaton/Hackaton.Core/Repository/TestRepository.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dlp.Connectors;
 
 namespace Hackaton.Core.Repository {
     public class TestRepository : BaseRepository, ITestRepository {
+
+
+        private const string GET_MERCHANTORDERID_QUERY =
+            @"SELECT MerchantOrderId FROM CreditCardTransaction 
+             INNER JOIN Merchant ON Merchant.MerchantId = MerchantOrder.MerchantId
+             WHERE 
+             MerchantKey = merchantKey
+             AND CrediCardTransactionKey = @CreditCardTransactionKey";
 
         public TestRepository(string connectionString)
             : base(connectionString) {
 
         }
 
-        public long GetMerchantOrderId(long merchantOrderId) {
-            throw new NotImplementedException();
+        /// <summary>
+        /// Obtém o merchantOrderId a partir de uma TransactionKey.
+        /// </summary>
+        /// <param name="creditCardTransactionKey"></param>
+        /// <returns></returns>
+        public long GetMerchantOrderId(Guid creditCardTransactionKey, Guid merchantKey) {
+
+            long merchantOrderId = 0;
+            using (DatabaseConnector dbConnector = new DatabaseConnector(base.connectionString)) {
+                
+                dbConnector.ExecuteReader<long>(GET_MERCHANTORDERID_QUERY, new {
+                    CreditCardTransactionKey = creditCardTransactionKey,
+                    MerchantKey = merchantKey
+                });
+            }
+
+            return merchantOrderId;
         }
 
         public void GetCreditCardTransactionData(long merchantOrderId) {
